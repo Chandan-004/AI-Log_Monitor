2c923be7d63d9bfd69edcb45b2dc3096cc0e356f
Chandan-004 <chandanlmp1607@gmail.com>
2025-11-26 22:55:29 +0530

    fixed database url for render

--- src/constants.js
+++ src/constants.js
@@ -1,14 +1,7 @@
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
 export const DB_CONFIG = {
-    user: "postgres",
-    host: "localhost",
-    database: "myapp_db",
-    port: 5432,
-    password: process.env.DB_PASSWORD,
-    ssl: false 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 };
-
-
-
