0bbe9033d79eba03ff10b985e1d10a307b59e680
Chandan-004 <chandanlmp1607@gmail.com>
2025-11-28 14:53:09 +0530

    feat: basic frontend added to navigate

--- src/index.js
+++ src/index.js
@@ -13,8 +13,30 @@ createLogsTable()
 import usersRouter from "./routes/user.routes.js"; 
 import logsRouter from "./routes/log.routes.js"; 
 
+app.get("/", (req, res) => {
+  res.send(`
+    <h1> AI Log Monitoring & Alerting System — Backend Running</h1>
+    <p>This project doesn't have a UI here. You can test it using Postman or REST APIs.</p>
+    <hr />
+    <p>Available endpoints:</p>
+    <ul>
+      <li>POST /api/v1/users/register</li>
+      <li>POST /api/v1/users/login</li>
+      <li>GET  /api/v1/users/profile</li>
+      <br />
+      <li>POST /api/v1/logs</li>
+      <li>GET  /api/v1/logs</li>
+      <li>PATCH /api/v1/logs/:id</li>
+      <li>DELETE /api/v1/logs/:id</li>
+    </ul>
+    <p>📌 Tip: Use the Authorization Bearer Token for protected routes.</p>
+  `);
+});
+
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/logs', logsRouter)
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
